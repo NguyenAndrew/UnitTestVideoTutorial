f56ee7116a1e7cc76563a247a2d9e02fbf0fbbca
NguyenAndrew <anguyen29@wisc.edu>
2020-01-24 23:33:15 -0600

    Remove duplicate unit tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,8 +6,6 @@ import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.*;
 
 class CalculatorTest {
-
-
     private static final int SAMPLE_ADDITION_OUTPUT = 0;
     private static final int SAMPLE_MULTIPLICATION_OUTPUT = 0;
     private static final int SAMPLE_SUBTRACTION_OUTPUT = 0;
@@ -30,52 +28,10 @@ class CalculatorTest {
     }
 
     @Test
-    public void givenInput10_whenCalculatorCalculates_thenWeExpectOutput140() {
+    public void givenAnInput_whenCalculatorCalculates_thenWeExpectAnOutput() {
         // Given (Setup)
         int expected = 140;
         int x = 10;
-        when(additionService.add(anyInt(), anyInt())).thenReturn(SAMPLE_ADDITION_OUTPUT);
-        when(multiplicationService.multiply(anyInt(), anyInt())).thenReturn(SAMPLE_MULTIPLICATION_OUTPUT);
-        when(subtractionService.subtract(anyInt(), anyInt())).thenReturn(expected);
-
-        // When (Run the thing that you want to test)
-        int y = calculator.calculateY(x);
-
-        // Then (Asserting what you want to be true, is actually true)
-        assertEquals(expected, y);
-
-        // Verify
-        verify(additionService, times(1)).add(anyInt(), anyInt());
-        verify(multiplicationService, times(1)).multiply(anyInt(), anyInt());
-        verify(subtractionService, times(1)).subtract(anyInt(), anyInt());
-    }
-
-    @Test
-    public void givenInput5_whenCalculatorCalculates_thenWeExpectOutput45() {
-        // Given (Setup)
-        int expected = 45;
-        int x = 5;
-        when(additionService.add(anyInt(), anyInt())).thenReturn(SAMPLE_ADDITION_OUTPUT);
-        when(multiplicationService.multiply(anyInt(), anyInt())).thenReturn(SAMPLE_MULTIPLICATION_OUTPUT);
-        when(subtractionService.subtract(anyInt(), anyInt())).thenReturn(expected);
-
-        // When (Run the thing that you want to test)
-        int y = calculator.calculateY(x);
-
-        // Then (Asserting what you want to be true, is actually true)
-        assertEquals(expected, y);
-
-        // Verify
-        verify(additionService, times(1)).add(anyInt(), anyInt());
-        verify(multiplicationService, times(1)).multiply(anyInt(), anyInt());
-        verify(subtractionService, times(1)).subtract(anyInt(), anyInt());
-    }
-
-    @Test
-    public void givenInputNegative10_whenCalculatorCalculates_thenWeExpectOutput60() {
-        // Given (Setup)
-        int expected = 60;
-        int x = -10;
         when(additionService.add(anyInt(), anyInt())).thenReturn(SAMPLE_ADDITION_OUTPUT);
         when(multiplicationService.multiply(anyInt(), anyInt())).thenReturn(SAMPLE_MULTIPLICATION_OUTPUT);
         when(subtractionService.subtract(anyInt(), anyInt())).thenReturn(expected);
