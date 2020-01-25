2d927630f92549b0b5ea081837093de4f1084d55
NguyenAndrew <anguyen29@wisc.edu>
2020-01-24 22:08:22 -0600

    Create calculator for our equation

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,10 @@
 public class Application {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
         int x = 10;
-        int y = (5 + x) * x - x;
+//        int y = (5 + x) * x - x;
+        int y = calculator.calculateY(x);
 
         System.out.println("My input X = " + x);
         System.out.println("My output Y = " + y);
---
+++ src/main/java/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+
+    public int calculateY(int x) {
+        int y = (5 + x) * x - x;
+        return y;
+    }
+}
