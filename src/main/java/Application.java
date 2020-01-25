6792cd2f15bbf1bb23fc57e6a3f5f8445b323976
NguyenAndrew <anguyen29@wisc.edu>
2020-01-24 22:02:21 -0600

    Solve the initial equation

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = (5 + x) * x - x;
+
+        System.out.println("My input X = " + x);
+        System.out.println("My output Y = " + y);
+    }
+}
