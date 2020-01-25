33c983f59b042c22067c7ea7776cf5a756d91657
NguyenAndrew <anguyen29@wisc.edu>
2020-01-24 22:28:15 -0600

    Create more tests for calculator calculate

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -25,6 +25,32 @@ class CalculatorTest {
         assertEquals(expected, y);
     }
 
+    @Test
+    public void givenInput5_whenCalculatorCalculates_thenWeExpectOutput45() {
+        // Given (Setup)
+        int expected = 45;
+        int x = 5;
+
+        // When (Run the thing that you want to test)
+        int y = calculator.calculateY(x);
+
+        // Then (Asserting what you want to be true, is actually true)
+        assertEquals(expected, y);
+    }
+
+    @Test
+    public void givenInputNegative10_whenCalculatorCalculates_thenWeExpectOutput60() {
+        // Given (Setup)
+        int expected = 60;
+        int x = -10;
+
+        // When (Run the thing that you want to test)
+        int y = calculator.calculateY(x);
+
+        // Then (Asserting what you want to be true, is actually true)
+        assertEquals(expected, y);
+    }
+
 
 
 }
